6c7f08dd8d82a5ed89b5dc711938d0593c177248
afkan_guseynov <af.kan@bk.ru>
2022-01-15 23:39:00 +0500

    0. Что такое обобщенные типы (generics)

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class Generics {
     public static void main(String[] args) {
         Generics gen = new Generics();

df2cb7b59dd948001a25e5e94552939141ef6f05
afkan_guseynov <af.kan@bk.ru>
2022-03-15 21:28:10 +0500

    4. Переопределить только equals [#1004 #275650]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -28,15 +28,16 @@ public class User {
     }
 
     public static void main(String[] args) {
-        GregorianCalendar birthday1 = new GregorianCalendar(2022, Calendar.FEBRUARY, 1, 1, 1, 1);
+       GregorianCalendar birthday1 = new GregorianCalendar(2022, Calendar.FEBRUARY, 1, 1, 1, 1);
         GregorianCalendar birthday2 = new GregorianCalendar(2022, Calendar.FEBRUARY, 1, 1, 1, 1);
         User user1 = new User("Tom", 1, birthday1);
         User user2 = new User("Tom", 1, birthday2);
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
-        for (User user : map.keySet()) {
-            System.out.println(map);
-        }
+        System.out.println(map.entrySet());
+        System.out.println(user1.equals(user2));
+        System.out.println(map.get(user1).hashCode());
+        System.out.println(map.get(user2).hashCode());
     }
 }

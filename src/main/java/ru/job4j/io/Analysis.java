d96e0a81b2d639f4dc6b8f548ac6bebb7250b72a
afkan_guseynov <af.kan@bk.ru>
2023-06-09 00:20:06 +0400

    8.Тестирование IO

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -2,10 +2,14 @@ package ru.job4j.io;
 
 import java.io.*;
 
+/**
+ * Класс Analysis предоставляет метод для анализа файла лога сервера и определения периодов,
+ * когда сервер был недоступен на основе кодов состояния.
+ */
 public class Analysis {
     public void unavailable(String source, String target) {
-        try  (BufferedReader in = new BufferedReader(new FileReader(source));
-              BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
             var builder = new StringBuilder();
             String line;
             while ((line = in.readLine()) != null) {
@@ -14,9 +18,9 @@ public class Analysis {
                     builder.append(list[1]).append(";");
                 }
                 if (builder.length() != 0 && (list[0].equals("200") || list[0].equals("300"))) {
-                    out.write(builder + list[1]);
+                    out.write(builder.append(list[1]).append(";").toString());
                     out.newLine();
-                    builder = new StringBuilder();
+                    builder.setLength(0);
                 }
             }
         } catch (IOException ex) {

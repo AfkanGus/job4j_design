efa77ab6302ae33be27480d8f8f8548d13ba7ef2
afkan_guseynov <af.kan@bk.ru>
2023-06-10 19:45:32 +0400

    7. Анализ доступности сервера

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,2 +1,34 @@
-package ru.job4j.io;public class Dir {
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Класс вывода содержимого директории и размеров файлов.
+ */
+public class Dir {
+    /**
+     * Главный метод класса.
+     *
+     * @param args аргументы командной строки.
+     * @throws IOException выбрасывает исключение при возникают проблемы доступа к файловой системе.
+     */
+    public static void main(String[] args) throws IOException {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("No exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("No directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("Directory : %s", file.getAbsolutePath()));
+      for (File subfile : file.listFiles()) {
+            if (subfile.isFile()) {
+                System.out.println(String.format("Name : %s, Size: %d bytes ", subfile.getName(), subfile.length()));
+            } else {
+                System.out.println(String.format("Name: %s", subfile.getName()));
+            }
+        }
+    }
 }

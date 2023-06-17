9ee36d20393f68750bba6286e9a24fafe60843e0
afkan_guseynov <af.kan@bk.ru>
2023-06-17 12:19:42 +0400

    12. Валидация параметров запуска [#246865 # [#246865 #423610]]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+
 /**
  * Программа для поиска файлов с заданным расширением в указанной директории.
  */
@@ -14,13 +15,14 @@ public class Search {
      * Метод, с которого начинается выполнение программы.
      *
      * @param args аргументы командной строки: [0] - начальная папка, [1] - расширение файлов.
-     * @throws IOException              если происходит ошибка ввода-вывода при поиске файлов.
+     * @throws IOException если происходит ошибка ввода-вывода при поиске файлов.
      */
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
         validator(args);
     }
+
     /**
      * Метод для поиска файлов, удовлетворяющих заданному условию, в указанной директории и её поддиректориях.
      *
@@ -34,6 +36,7 @@ public class Search {
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
+
     /**
      * Метод для валидации аргументов командной строки.
      *
@@ -41,11 +44,14 @@ public class Search {
      * @throws IllegalArgumentException если переданы некорректные аргументы командной строки.
      */
     public static void validator(String[] args) {
+        if (!Files.exists(Paths.get(args[0])) || !Files.isDirectory(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Первый параметр - путь к существующему каталогу");
+        }
+        if (!args[1].startsWith(".") || args[1].length() == 1) {
+            throw new IllegalArgumentException("Второй параметр - расширение файла, начинающееся с символа '.' и содержащее дополнительные символы.");
+        }
         if (args.length != 2) {
             throw new IllegalArgumentException("Программа должна запускаться с двумя параметрами.");
-        }
-        if (!args[1].startsWith(".java")) {
-            throw new IllegalArgumentException("Второй параметр - расширение файлов java");
         }
     }
 }

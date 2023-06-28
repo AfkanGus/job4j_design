611f28cba5264fab832ea797296424daddb5b906
afkan_guseynov <af.kan@bk.ru>
2023-06-28 21:44:23 +0400

    16. Кодировка

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.util.List;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+
+    }
+
+    /**
+     * readPhrases(), читает фразы из файла;
+     *
+     * @return
+     */
+    private List<String> readPhrases() {
+        return null;
+    }
+
+    /**
+     * - saveLog(), сохраняет лог чата в файл.
+     *
+     * @param log
+     */
+    private void saveLog(List<String> log) {
+
+    }
+
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("", "");
+        cc.run();
+    }
+}
---
+++ src/main/java/ru/job4j/io/UsageEncode.java
@@ -0,0 +1,45 @@
+package ru.job4j;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class UsageEncode {
+    public String readFiles(String path) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader bufferedReader =
+                     new BufferedReader(new FileReader(path))) {
+            bufferedReader.lines().map(
+                    s -> s + System.lineSeparator()).forEach(builder::append);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    public void writeDataInFile(String path, List<String> data) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path,
+                Charset.forName("WINDOWS-1251"), true))) {
+            data.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = "./data/text.txt";
+        UsageEncode encode = new UsageEncode();
+        List<String> strings = List.of(
+                "Новая строка 1",
+                "Новая строка 2",
+                "Новая строка 3",
+                "Новая строка 4",
+                "Новая строка 5"
+        );
+            encode.writeDataInFile(path, strings);
+        String s = encode.readFiles(path);
+        System.out.println("Data form files: ");
+        System.out.println(s);
+    }
+}

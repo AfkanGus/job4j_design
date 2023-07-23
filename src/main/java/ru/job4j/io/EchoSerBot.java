4e26c778eebbc1a9827b128051a17934a6b7d5a0
AfkanGus <af.kan@bk.ru>
2023-07-23 23:37:23 +0400

    3. Бот [#7921 # [#7921]]

---
+++ src/main/java/ru/job4j/io/EchoSerBot.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 3. Бот [#7921 # [#7921].
+ * Этот класс представляет простого HTTP-бота (EchoBot).
+ * Он принимает запросы от клиентов по протоколу HTTP и отвечает на них.
+ * Если клиент отправляет запрос с параметром "?msg=Hello", сервер отвечает "Hello".
+ * Если клиент отправляет запрос с параметром "?msg=Exit", сервер отвечает
+ * "Server is closing!" и закрывает свой серверный сокет.
+ * В остальных случаях сервер отвечает "What".
+ */
+public class EchoSerBot {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(9000)) {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                try (OutputStream outputStream = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream())
+                     )) {
+                    StringBuilder reqFromClient = new StringBuilder();
+                    outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        System.out.println(str);
+                        reqFromClient.append(str).append("\r\n");
+                    }
+                    /*ответ сервера*/
+                    String response;
+                    /*запрос клиента*/
+                    String request = reqFromClient.toString();
+                    if (request.contains("?msg=Hello")) {
+                        response = "Hello";
+                    } else if (request.contains("?msg=Exit")) {
+                        response = "Server is closing!";
+                        serverSocket.close();
+                    } else {
+                        response = "What";
+                    }
+                    outputStream.write(("HTTP/1.1 200 OK\r\n\r\n" + response).getBytes());
+                    outputStream.flush();
+                }
+            }
+        }
+    }
+}
+

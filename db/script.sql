a1754bb8db84522e3c17a4d11ae22b53e2364f0c
AfkanGus <af.kan@bk.ru>
2023-11-12 01:11:42 +0500

    0. Что такое база данных. [#6860]

---
+++ db/script.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee(profession, salary, working_hours)
+VALUES('Teacher', 335.5, 160);
+SELECT * FROM employee;
+UPDATE employee SET salary = 400, working_hours = 180;
+SELECT * FROM employee;
+DELETE FROM employee;
+SELECT * FROM employee;
--- db/testsql.sql
+++
@@ -1,6 +0,0 @@
-create table animals (
-    id serial primary key,
-    area text
-);
-insert into role (name) values ('petr');
-select * from role;

e47d4d96f685f88e436d8151efb32d0b401ce901
afkan_guseynov <af.kan@bk.ru>
2024-02-06 16:59:53 +0500

    3. Поиск файлов по критерию [#783 #478400]

--- src/main/java/ru/job4j/io/searchkriteria/CommandLineArgs.java
+++ src/main/java/ru/job4j/io/searchkriteria/CommandLineArgs.java
@@ -1,5 +1,6 @@
 package ru.job4j.io.searchkriteria;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -35,7 +36,25 @@ public class CommandLineArgs {
         return commandLineArguments.get("-o");
     }
 
-    public String getFileNameForSearch() {
-        
+    public void getFileNameForSearch() {
+        // Преобразовываем массив args в поток строк
+        Arrays.stream(args)
+                // Фильтруем только аргументы, начинающиеся с символа "-"
+                .filter(arg -> arg.startsWith("-"))
+                // Преобразуем аргументы в пары ключ-значение
+                .forEach(arg -> {
+                    String key = arg;
+                    String value = null;
+                    // Получаем индекс аргумента в массиве
+                    int index = Arrays.asList(args).indexOf(arg);
+                    // Если следующий аргумент существует и не начинается с "-", он является значением
+                    if (index + 1 < args.length && !args[index + 1].startsWith("-")) {
+                        value = args[index + 1];
+                    }
+                    // Убираем символ "-" из ключа
+                    key = key.substring(1);
+                    // Помещаем пару ключ-значение в Map
+                    commandLineArguments.put(key, value);
+                });
     }
 }
--- src/main/java/ru/job4j/io/searchkriteria/SearchArgs.java
+++ src/main/java/ru/job4j/io/searchkriteria/SearchArgs.java
@@ -1,5 +1,13 @@
 package ru.job4j.io.searchkriteria;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class SearchArgs {
 
 }

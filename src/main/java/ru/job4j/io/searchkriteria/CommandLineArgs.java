d02f15163c6f4ad2179ec57c67ed95c6e0b5b65e
afkan_guseynov <af.kan@bk.ru>
2024-02-10 01:10:04 +0500

    3. Поиск файлов по критерию [#783 #478400]

--- src/main/java/ru/job4j/io/searchkriteria/CommandLineArgs.java
+++ src/main/java/ru/job4j/io/searchkriteria/CommandLineArgs.java
@@ -1,9 +1,7 @@
 package ru.job4j.io.searchkriteria;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class CommandLineArgs {
     private Map<String, String> commandLineArguments = new HashMap<>();
@@ -18,43 +16,17 @@ public class CommandLineArgs {
     }
 
     public void initializeArgs() {
-        IntStream.range(0, args.length)
-                .filter(i -> args[i].startsWith("-"))
-                .forEach(i -> {
-                    String key = args[i];
-                    int index = i;
-                    String value = (index + 1 < args.length && !args[index + 1].startsWith("-")) ? args[index + 1] : null;
-                    commandLineArguments.put(key, value);
-                });
-    }
-
-    public String getDirectory() {
-        return commandLineArguments.get("-d");
-    }
-
-    public String getResultFileName() {
-        return commandLineArguments.get("-o");
-    }
-
-    public void getFileNameForSearch() {
-        // Преобразовываем массив args в поток строк
-        Arrays.stream(args)
-                // Фильтруем только аргументы, начинающиеся с символа "-"
-                .filter(arg -> arg.startsWith("-"))
-                // Преобразуем аргументы в пары ключ-значение
-                .forEach(arg -> {
-                    String key = arg;
-                    String value = null;
-                    // Получаем индекс аргумента в массиве
-                    int index = Arrays.asList(args).indexOf(arg);
-                    // Если следующий аргумент существует и не начинается с "-", он является значением
-                    if (index + 1 < args.length && !args[index + 1].startsWith("-")) {
-                        value = args[index + 1];
-                    }
-                    // Убираем символ "-" из ключа
-                    key = key.substring(1);
-                    // Помещаем пару ключ-значение в Map
-                    commandLineArguments.put(key, value);
-                });
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                String key = args[i];
+                String value = null;
+                if (i + 1 < args.length && !args[i + 1].startsWith("-")) {
+                    value = args[i + 1];
+                    i++;
+                }
+                commandLineArguments.put(key, value);
+            }
+        }
     }
 }
+
--- src/main/java/ru/job4j/io/searchkriteria/SearchArgs.java
+++ src/main/java/ru/job4j/io/searchkriteria/SearchArgs.java
@@ -1,13 +1,72 @@
 package ru.job4j.io.searchkriteria;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.function.Predicate;
 
 public class SearchArgs {
+    private List<File> list = new ArrayList<>();
+    /*чтобы рекурсивно обойти все поддиректории.*/
+    private Queue<File> fileQueue = new PriorityQueue<>();
 
+    public List<File> filesDirectory(String parentDir, Predicate<File> predicate) {
+        /* создал ссылку на путь */
+        File parentFile = new File(parentDir);
+        fileQueue.offer(parentFile);
+        /*проверим пустоту очерди*/
+        while (!fileQueue.isEmpty()) {
+            File headObjectDir = fileQueue.poll();
+            if (headObjectDir.isDirectory()) {
+                File[] files = headObjectDir.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        fileQueue.offer(file);
+                    }
+                }
+            } else {
+                list.add(headObjectDir);
+            }
+        }
+        return list;
+    }
+
+    public List<File> searchByMask(String parent, List<String> extension) {
+        List<File> resultFiles = new ArrayList<>();
+        SearchArgs searchArgs = new SearchArgs();
+        /*Создаем предикат для проверки расширения файла*/
+        Predicate<File> extensionPredicate = new Predicate<File>() {
+            @Override
+            public boolean test(File file) {
+                String fileName = file.getName();
+                int lastDotIndex = fileName.lastIndexOf('.');
+                if (lastDotIndex != -1 && lastDotIndex != fileName.length() - 1) {
+                    String fileExtension = fileName.substring(lastDotIndex + 1);
+                    return extension.contains(fileExtension);
+                }
+                return false;
+            }
+        };
+        /* Получаем список файлов в директории, удовлетворяющих предикату extensionPredicate*/
+        List<File> allFiles = searchArgs.filesDirectory(parent, extensionPredicate);
+        resultFiles.addAll(allFiles);
+        return resultFiles;
+    }
+
+    public List<File> searchByName(String parent, String name) {
+        Predicate<File> predicate = new Predicate<File>() {
+            @Override
+            public boolean test(File file) {
+                String fileName = file.getName();
+                int extensionIndex = fileName.lastIndexOf('.');
+                String nameWithoutExtension = extensionIndex >= 0 ? fileName.substring(0, extensionIndex) : fileName;
+                return name.equals(nameWithoutExtension);
+            }
+        };
+        return filesDirectory(parent, predicate);
+    }
 }
+
+

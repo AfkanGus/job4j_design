40144292e925e114e1cca10e10b7d3a1272ce925
AfkanGus <af.kan@bk.ru>
2024-02-28 14:07:59 +0500

    5. Итератор не-null значений [#505081].

---
+++ src/main/java/ru/job4j/iterator/NonNullIterator.java
@@ -0,0 +1,58 @@
+package ru.job4j.iterator;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 5. Итератор не-null значений [#505081].
+ * Итератор должен принимать массив произвольных объектов Integer.
+ * Итератор, проходит по массиву data и возвращает только ненулевые элементы.
+ */
+public class NonNullIterator implements Iterator<Integer> {
+    private Integer[] data;
+    private int index;
+
+    public NonNullIterator(Integer[] data) {
+        this.data = data;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (index < data.length && data[index] == null) {
+            index++;
+        }
+        return index < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+}
+   /* private final Iterator<Integer> iterator;
+
+    public NonNullIterator(Integer[] data) {
+        Stream<Integer> stream = Arrays.stream(data).filter(x -> x != null);
+        this.iterator = stream.iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return iterator.next();
+    }*/
+

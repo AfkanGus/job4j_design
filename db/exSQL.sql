20e1f14ff43c4702a1eeae4ec469ac277bb0e1bf
afkan_guseynov <af.kan@bk.ru>
2024-03-17 18:44:51 +0500

    exSQL

--- db/exSQL.sql
+++ db/exSQL.sql
@@ -115,38 +115,72 @@ select c.id,c.model,e.volume,e.power from cars as c join engines e on c.id = e.c
 -------------------------------------------------------------------------------------------------------------------
 
 --- мы столкнемся с тем, что будут столбцы в разных таблицах иметь одинаковые имена.
-CREATE TABLE accounts (
+create table accounts (
     id int primary key,
     email text,-- Одинаковое поле
     password text
 );
 
-CREATE TABLE users (
+create table users (
     id int primary key,
     email text,-- Одинаковое поле
     "name" text,
     account_id int references accounts(id)
 );
-INSERT INTO accounts VALUES (1, 'for_auth_1@mail.ru', '12345');
-INSERT INTO accounts VALUES (2, 'for_auth_1@bk.ru', '67890');
+insert into accounts values (1, 'for_auth_1@mail.ru', '12345');
+insert into accounts values (2, 'for_auth_1@bk.ru', '67890');
 
-INSERT INTO users VALUES (1, 'email_1@mail.ru', 'first_name', 1);
-INSERT INTO users VALUES (2, 'email_2@bk.ru', 'second_name', 2);
+insert into users values (1, 'email_1@mail.ru', 'first_name', 1);
+insert into users values (2, 'email_2@bk.ru', 'second_name', 2);
 --можно указывать AS для присвоения псевдонима
 --для идентификации столбцов указывать псевдоним необходимо только там, где имена столбцов совпадают.
 select a.id,a.email,a.password, u.email,u.name from accounts as a inner join users u on a.id = u.account_id;
 
+----3. Фильтры [#1732].
+create table type(
+id int primary key,
+"name" text
+);
 
+create table product(
+id int primary key,
+"name" text,
+expired_date timestamp,
+price int,
+type_id int references type(id)
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
+insert into type values (1,'Сыр'),(2,'Молоко'),(3,'Мороженое');
+insert into product values (1,'Сыр плавленный','2024-01-15',150,1);
+insert into product values (2,'Сыр моцарелла','2024-02-15',300,1);
+insert into product values (3,'Сыр Российский','2024-03-16',100,1);
+insert into product values (4,'Молоко Летний луг','2024-03-16',50,2);
+insert into product values (5,'Молоко Коровье','2024-01-15',60,2);
+insert into product values (6,'Мороженое обычное','2024-03-16',30,3);
+insert into product values (7,'Мороженое ванильное','2024-03-16',30,3);
+insert into product values (8,'Мороженое чешское','2024-03-16',30,3);
+insert into product values (9,'Мороженое польское','2024-03-16',31,3);
+insert into product values (10,'Мороженое красное','2024-03-16',30,3);
+insert into product values (11,'Мороженое фаворит','2024-03-16',30,3);
+insert into product values (12,'Мороженое карона','2024-03-16',30,3);
+insert into product values (14,'Мороженое жара','2024-03-16',30,3);
+insert into product values (15,'Мороженое новогоднее','2024-03-16',30,3);
+insert into product values (16,'Мороженое стужа','2024-03-16',30,3);
+insert into product values (17,'Мороженое караван','2024-03-16',30,3);
+-- 1. Запрос на получение всех продуктов с типом 'Сыр'.
+select * from product inner join type on product.type_id = type.id where type.name = 'Сыр';
+-- 2. Запрос на получение всех продуктов c именем "мороженое".
+select * from product where product.name like  '%Мороженое%';
+-- 3. Запрос на вывод товаров с истекшим сроком годности.
+select * from product where product.expired_date < current_date;
+-- 4. Запрос который выводит самый дорогой продукт по типам.
+select * from product where price =( select max(price) from product);
+ -- 5.Вывод количество продуктов для каждого типа.
+select type.name Имя_типа, count(product.name) Количество from product inner join type on product.type_id = type.id group by type.name;
+-- 6. Получение всех продуктов с типом сыр и молоко
+select * from product inner join type on product.type_id = type.id where type.name in('Сыр','Молоко');
+-- 7. Запрос, на вывод типов продуктов, которых осталось меньше 10 штук.
+select t.name as Тип_товара from product p join type t on p.type_id = t.id group by t.id, t.name having count(p.type_id) < 10;
+--8. Вывести все продукты и их тип.
+select  p.name Наименования_товара, p.price Цена_товара, p.expired_date Срок_годности_товара, t.name Тип_товара
+ from product p join type t on p.type_id = t.id;

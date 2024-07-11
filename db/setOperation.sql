c80053a6311ea3d018b8c2fdb27a395518593e25
afkan_guseynov <af.kan@bk.ru>
2024-07-11 18:32:29 +0500

    10. Операторы множества в SQL [#504896]

---
+++ db/setOperation.sql
@@ -0,0 +1,44 @@
+--10. Операторы множества в SQL [#504896]
+CREATE TABLE movie
+(
+    id       SERIAL PRIMARY KEY,
+    "name"     text,
+    director text
+);
+
+CREATE TABLE book
+(
+    id     SERIAL PRIMARY KEY,
+    title  text,
+    author text
+);
+
+INSERT INTO movie (name, director)
+VALUES ('Марсианин', 'Ридли Скотт'),
+       ('Матрица', 'Братья Вачовски'),
+       ('Властелин колец', 'Питер Джексон'),
+       ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
+       ('Железный человек', 'Джон Фавро');
+
+INSERT INTO book (title, author)
+VALUES ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+       ('Властелин колец', 'Джон Толкин'),
+       ('1984', 'Джордж Оруэлл'),
+       ('Марсианин', 'Энди Уир'),
+       ('Божественная комедия', 'Данте Алигьери');
+select * from movie;
+--- выведите названия всех фильмов, которые сняты по книге.
+select name from movie
+intersect select title from book;
+--- выведите все названия книг, у которых нет экранизации;
+select * from book;
+select title from book
+except
+select name from movie;
+--- выведите все уникальные названия произведений из таблиц movie и book (т.е фильмы, которые сняты не по книге, и книги без экранизации)
+select name from movie
+except
+select title from book
+union all
+(select title from book
+except select name from movie);

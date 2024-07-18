46b43475db3b56f1c52a2607cbfc4d156770fec6
afkan_guseynov <af.kan@bk.ru>
2024-07-18 23:18:26 +0500

    10. Операторы множества в SQL [#504896]

--- db/setOperation.sql
+++ db/setOperation.sql
@@ -1,27 +1,27 @@
 --10. Операторы множества в SQL [#504896]
-CREATE TABLE movie
+create table movie
 (
-    id       SERIAL PRIMARY KEY,
+    id       serial primary key,
     "name"     text,
     director text
 );
 
-CREATE TABLE book
+create table book
 (
-    id     SERIAL PRIMARY KEY,
+    id     serial primary key,
     title  text,
     author text
 );
 
-INSERT INTO movie (name, director)
-VALUES ('Марсианин', 'Ридли Скотт'),
+insert into movie (name, director)
+values ('Марсианин', 'Ридли Скотт'),
        ('Матрица', 'Братья Вачовски'),
        ('Властелин колец', 'Питер Джексон'),
        ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
        ('Железный человек', 'Джон Фавро');
 
-INSERT INTO book (title, author)
-VALUES ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+insert into book (title, author)
+values ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
        ('Властелин колец', 'Джон Толкин'),
        ('1984', 'Джордж Оруэлл'),
        ('Марсианин', 'Энди Уир'),

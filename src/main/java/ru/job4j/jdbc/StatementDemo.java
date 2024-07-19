7d09d7864725f17c738b1b400ee9c908d53291ca
afkan_guseynov <af.kan@bk.ru>
2024-07-19 19:04:27 +0500

    0.1. Statement[#379306].

---
+++ src/main/java/ru/job4j/jdbc/StatementDemo.java
@@ -0,0 +1,54 @@
+package ru.job4j.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.StringJoiner;
+
+/**
+ * 0.1. Statement[#379306].
+ * для операций в подключенной БД еть интерфейсы Statement, PreparedStatement.
+ */
+public class StatementDemo {
+    private static Connection getConnection() throws Exception {
+        /*Подключаемся к базе данных postgres*/
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String login = "postgres";
+        String password = "password";
+        return DriverManager.getConnection(url, login, password);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "CREATE TABLE IF NOT EXISTS demo_table(%s, %s);",
+                        "id SERIAL PRIMARY KEY",
+                        "name TEXT"
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "demo_table"));
+            }
+        }
+    }
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "SELECT * FROM %s LIMIT 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+}

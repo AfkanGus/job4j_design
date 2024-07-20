6f4bbad82190ffceb2d7df21bc7410fbbcf2a2cb
AfkanGus <af.kan@bk.ru>
2024-07-20 17:07:42 +0500

    0.1. Statement [#379306]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,42 +1,69 @@
 package ru.job4j.jdbc;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.StringJoiner;
 
+
 public class TableEditor implements AutoCloseable {
     private Connection connection;
-
     private Properties properties;
 
-    public TableEditor(Properties properties) {
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
         this.properties = properties;
         initConnection();
     }
 
-    private void initConnection() {
-        connection = null;
+    private void initConnection() throws SQLException, ClassNotFoundException {
+        String driver = properties.getProperty("hibernate.connection.driver_class");
+        String url = properties.getProperty("hibernate.connection.url");
+        String login = properties.getProperty("hibernate.connection.username");
+        String password = properties.getProperty("hibernate.connection.password");
+        Class.forName(driver);
+        connection = DriverManager.getConnection(url, login, password);
     }
 
-    /*создает пустую таблицу без столбцов с указанным именем*/
-    public void createTable(String tableName) {
+    public void createTable(String tableName) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "CREATE TABLE IF NOT EXISTS %s (id SERIAL PRIMARY KEY)", tableName);
+            statement.execute(sql);
+        }
     }
 
-    public void dropTable(String tableName) {
+    public void dropTable(String tableName) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "DROP TABLE IF EXISTS %s", tableName);
+            statement.execute(sql);
+        }
     }
 
-    public void addColumn(String tableName, String columnName, String type) {
+    public void addColumn(String tableName, String columnName, String type) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE %s ADD COLUMN %s %s", tableName, columnName, type);
+            statement.execute(sql);
+        }
     }
 
-    public void dropColumn(String tableName, String columnName) {
+    public void dropColumn(String tableName, String columnName) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE %s DROP COLUMN %s", tableName, columnName);
+            statement.execute(sql);
+        }
     }
 
-    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE %s RENAME COLUMN %s TO %s", tableName, columnName, newColumnName);
+            statement.execute(sql);
+        }
     }
-
 
     public String getTableScheme(String tableName) throws Exception {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
@@ -61,6 +88,35 @@ public class TableEditor implements AutoCloseable {
     public void close() throws Exception {
         if (connection != null) {
             connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("./src/main/resources/tableEditor.properties")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try (TableEditor editor = new TableEditor(properties)) {
+            String tableName = "test_table";
+            editor.createTable(tableName);
+            System.out.println(editor.getTableScheme(tableName));
+
+            editor.addColumn(tableName, "name", "TEXT");
+            System.out.println(editor.getTableScheme(tableName));
+
+            editor.renameColumn(tableName, "name", "full_name");
+            System.out.println(editor.getTableScheme(tableName));
+
+            editor.dropColumn(tableName, "full_name");
+            System.out.println(editor.getTableScheme(tableName));
+
+            editor.dropTable(tableName);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }

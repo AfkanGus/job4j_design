ea660e5a460e418cf147cd435af8605145aaac19
afkan_guseynov <af.kan@bk.ru>
2024-07-21 23:02:50 +0500

    1. Динамический список на массиве. [#158].

--- src/main/java/ru/job4j/collection/List.java
+++ src/main/java/ru/job4j/collection/List.java
@@ -1,5 +1,8 @@
 package ru.job4j.collection;
 
+import java.util.Collection;
+import java.util.ListIterator;
+
 /**
  * 1. Динамический список на массиве. [#158].
  */
@@ -13,4 +16,36 @@ public interface List<T> extends Iterable<T> {
     T get(int index);
 
     int size();
+
+    List<T> subList(int fromIndex, int toIndex);
+
+    boolean isEmpty();
+
+    boolean contains(Object o);
+
+    Object[] toArray();
+
+    <T1> T1[] toArray(T1[] a);
+
+    boolean remove(Object o);
+
+    boolean containsAll(Collection<?> c);
+
+    boolean addAll(Collection<? extends T> c);
+
+    boolean addAll(int index, Collection<? extends T> c);
+
+    boolean removeAll(Collection<?> c);
+
+    boolean retainAll(Collection<?> c);
+
+    void clear();
+
+    int indexOf(Object o);
+
+    int lastIndexOf(Object o);
+
+    ListIterator<T> listIterator();
+
+    ListIterator<T> listIterator(int index);
 }
--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -6,7 +6,7 @@ import java.util.*;
  * 1. Динамический список на массиве. [#158].
  * SimpleArrayList - класс список на основе динамического массива.
  *
- * @param <T> - тип даннных в контейнер.
+ * @param <T> - тип данных в контейнер.
  */
 public class SimpleArrayList<T> implements List<T> {
     private T[] container;
@@ -32,8 +32,9 @@ public class SimpleArrayList<T> implements List<T> {
     private void grow() {
         if (container.length == 0) {
             container = Arrays.copyOf(container, 1);
+        } else {
+            container = Arrays.copyOf(container, container.length * 2);
         }
-        container = Arrays.copyOf(container, container.length * 2);
         modCount++;
     }
 
@@ -41,7 +42,7 @@ public class SimpleArrayList<T> implements List<T> {
      * Метод будет добавлять объект в массив container.
      * Проверим равенство счетчика size и массива container.
      * Если они равны, то расширяет массив метод grow().
-     * Далее, добавляются объеты в container.
+     * Далее, добавляются объекты в container.
      * и инкремент счетчика modCount++.
      *
      * @param value - объект.
@@ -60,7 +61,7 @@ public class SimpleArrayList<T> implements List<T> {
      *
      * @param index    - указанный индекс.
      * @param newValue - новые объекты.
-     * @return преджный индекс.
+     * @return предыдущее значение.
      */
     @Override
     public T set(int index, T newValue) {
@@ -69,27 +70,26 @@ public class SimpleArrayList<T> implements List<T> {
         return prevValue;
     }
 
-
     /**
      * Метод удалит объекты в index.
      *
      * @param index-
-     * @return - prevIndex.
+     * @return - предыдущее значение.
      */
     @Override
     public T remove(int index) {
-        T prevIndex = get(index);
+        T prevValue = get(index);
         System.arraycopy(container, index + 1, container, index, size - index - 1);
         container[--size] = null;
         modCount++;
-        return prevIndex;
+        return prevValue;
     }
 
     /**
      * Метод получает объекты из указанного индекса.
      *
      * @param index - указанный индекс.
-     * @return - объкт с нужным index.
+     * @return - объект с нужным index.
      */
     @Override
     public T get(int index) {
@@ -106,6 +106,16 @@ public class SimpleArrayList<T> implements List<T> {
     public int size() {
         return size;
     }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        Objects.checkFromToIndex(fromIndex, toIndex, size);
+        List<T> sublist = (List<T>) new ArrayList<T>();
+        for (int i = fromIndex; i < toIndex; i++) {
+            sublist.add(container[i]);
+        }
+        return sublist;
+}
 
     @Override
     public Iterator<T> iterator() {
@@ -130,4 +140,224 @@ public class SimpleArrayList<T> implements List<T> {
             }
         };
     }
+
+    // Реализация остальных методов интерфейса List
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(container, size);
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        if (a.length < size) {
+            return (T1[]) Arrays.copyOf(container, size, a.getClass());
+        }
+        System.arraycopy(container, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int index = 0; index < size; index++) {
+            if (Objects.equals(o, container[index])) {
+                remove(index);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object e : c) {
+            if (!contains(e)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        for (T e : c) {
+            add(e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        Objects.checkIndex(index, size);
+        int numNew = c.size();
+        if (numNew == 0) {
+            return false;
+        }
+        if (numNew > container.length - size) {
+            grow();
+        }
+        int numMoved = size - index;
+        if (numMoved > 0) {
+            System.arraycopy(container, index, container, index + numNew, numMoved);
+        }
+        for (T e : c) {
+            container[index++] = e;
+        }
+        size += numNew;
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object e : c) {
+            while (remove(e)) {
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(container[i])) {
+                remove(i);
+                i--;
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            container[i] = null;
+        }
+        size = 0;
+        modCount++;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(o, container[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (Objects.equals(o, container[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return listIterator(0);
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return new ListIterator<>() {
+            private int cursor = index;
+            private int lastRet = -1;
+            int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                lastRet = cursor;
+                return container[cursor++];
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor > 0;
+            }
+
+            @Override
+            public T previous() {
+                if (!hasPrevious()) {
+                    throw new NoSuchElementException();
+                }
+                lastRet = --cursor;
+                return container[cursor];
+            }
+
+            @Override
+            public int nextIndex() {
+                return cursor;
+            }
+
+            @Override
+            public int previousIndex() {
+                return cursor - 1;
+            }
+
+            @Override
+            public void remove() {
+                if (lastRet < 0) {
+                    throw new IllegalStateException();
+                }
+                SimpleArrayList.this.remove(lastRet);
+                cursor = lastRet;
+                lastRet = -1;
+                expectedModCount = modCount;
+            }
+
+            @Override
+            public void set(T e) {
+                if (lastRet < 0) {
+                    throw new IllegalStateException();
+                }
+                SimpleArrayList.this.set(lastRet, e);
+            }
+
+            @Override
+            public void add(T e) {
+                int i = cursor;
+                SimpleArrayList.this.add(e);
+                cursor = i + 1;
+                lastRet = -1;
+                expectedModCount = modCount;
+            }
+        };
+    }
 }
+

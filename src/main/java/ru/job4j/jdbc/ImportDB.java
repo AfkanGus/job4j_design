fe729c7823b00dedb139cb4b1ea047a8dc421a79
AfkanGus <af.kan@bk.ru>
2024-07-24 00:53:04 +0500

    0.2. PreparedStatement [#379307]

---
+++ src/main/java/ru/job4j/jdbc/ImportDB.java
@@ -0,0 +1,66 @@
+package ru.job4j.jdbc;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+public class ImportDB {
+    private Properties config;
+    private String dump;
+
+    public ImportDB(Properties config, String dump) {
+        this.config = config;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(dump))) {
+            /* reader.lines().forEach(...); */
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(config.getProperty("jdbc.driver"));
+        try (Connection connection = DriverManager.getConnection(
+                config.getProperty("jdbc.url"),
+                config.getProperty("jdbc.username"),
+                config.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users ...")) {
+                    preparedStatement.setString(1, user.name);
+                    preparedStatement.setString(2, user.email);
+                    preparedStatement.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties config = new Properties();
+        try <span>(InputStream </span><span>input </span><span>= </span>ImportDB.<span>class</span><span>
+    .getClassLoader</span><span>()</span><span>
+    .getResourceAsStream</span><span>(</span><span>"app.properties"</span><span>)</span><span>) </span><span>{</span><span>
+</span>            config.load(input);
+        }
+        ImportDB dataBase = new ImportDB(config, "./dump.txt");
+        dataBase.save(dataBase.load());
+    }
+}

99826a1e0349b7b0a144b020bda90083eb95b29c
afkan_guseynov <af.kan@bk.ru>
2024-11-19 18:48:30 +0500

    1. Виды сборщиков мусора [#6852 #520553].

--- src/main/java/ru/job4j/memory/GCTypeDemo.java
+++ src/main/java/ru/job4j/memory/GCTypeDemo.java
@@ -12,7 +12,7 @@ public class GCTypeDemo {
         String[] data = new String[1_0];
         for (int i = 0; ; i = (i + 1) % data.length) {
             /*внтр цкл бдт сздвтс нв стрк в data[i]*/
-            data[i] = String.valueOf( /*сздм слчн чсл до 255,првдм к char
+            data[i] = String.valueOf(/*сздм слчн чсл до 255,првдм к char
             зтм прбрзм эт смвл в стрк*/
                     (char) random.nextInt(255)
             ).repeat(length); /*Этт мтд пвтрт стрк length раз сздв длн стрк*/
